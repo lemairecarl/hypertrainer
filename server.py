7b78cffbf42b7f3ab91873d4e0216062d7bd1a76
Carl Lemaire <c.lemaire@live.ca>
2019-02-18 11:23:23 -0500

    Show bar plots for classwise score metrics

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from collections import namedtuple
 
 import numpy as np
+import pandas as pd
 from ruamel_yaml import YAML
 import visdom
 
@@ -14,53 +15,70 @@ yaml = YAML()
 CONFIGS_PATH = Path(os.environ['EM_CONFIGS_PATH'])
 
 Task = namedtuple('Task', ['name', 'metrics'])
-Metric = namedtuple('Metric', ['name', 'data'])
+Metric = namedtuple('Metric', ['name', 'type', 'data'])
 
 
 class Server(object):
     def __init__(self):
         self.vis = visdom.Visdom()
+        self.tasks = []
         
         if not self.vis.check_connection():
             raise RuntimeError('A visdom server must be running.')
     
     @staticmethod
-    def get_metrics(output_path: Path):
+    def get_metrics(config_data):
         # TODO train + val
         metrics = []
+        output_path = Path(get_item_at_path(config_data, 'training.output_path'))
         
         # Loss
         losses = np.loadtxt(str(output_path / 'trn_losses_values.log'), delimiter=' ')
         metrics.append(
-            Metric('Loss', losses)
+            Metric(name='Loss', type='line', data=losses)
         )
         
-        # TODO other metrics
+        # Classwise final score
+        all_scores = pd.read_csv(output_path / 'trn_classes_score.log', sep=' ', header=None, names=['ep', 'id', 'val'])
+        scores = all_scores[all_scores['ep'] == all_scores['ep'].max()].copy()
+        # TODO keep best epoch instead of last
+        scores['metric'], scores['class_idx'] = scores['id'].str.split('_', 1).str
+        for name, val in scores.groupby('metric'):
+            metrics.append(
+                Metric(name=name, type='bar', data=val['val'].values)
+            )
+        
         return metrics
     
-    def get_tasks(self):
+    def retrieve_task(self, config_file_path):
+        config_file_path = Path(config_file_path)
+        config_data = yaml.load(config_file_path)
+        return Task(config_file_path.stem, self.get_metrics(config_data))
+    
+    def refresh_tasks(self):
         """
         TODO documentation
         :return: a list of Task objects.
         """
-        
+
+        self.tasks.clear()
         # Iterate on yaml files
-        tasks = []
         for config_file_path in CONFIGS_PATH.glob('*.yaml'):
-            config_file_path = Path(config_file_path)
-            config_data = yaml.load(config_file_path)
-            output_path = Path(get_item_at_path(config_data, 'training.output_path'))
-            tasks.append(
-                Task(config_file_path.stem, self.get_metrics(output_path))
-            )
-        return tasks
+            self.tasks.append(self.retrieve_task(config_file_path))
     
     def main_loop(self):
         print('Experiment Manager Server is running.')
         while True:
-            for task in self.get_tasks():
+            self.refresh_tasks()
+            for task in self.tasks:
                 for m in task.metrics:
-                    self.vis.line(Y=m.data[:, 1], X=m.data[:, 0], win=m.name, env=task.name, opts=dict(title=m.name))
+                    if m.type == 'line':
+                        self.vis.line(Y=m.data[:, 1], X=m.data[:, 0],
+                                      win=m.name, env=task.name, opts=dict(title=m.name))
+                    elif m.type == 'bar':
+                        rn = [str(i) for i in range(len(m.data))]
+                        self.vis.bar(m.data,
+                                     win=m.name, env=task.name, opts=dict(title=m.name, rownames=rn))
             
             time.sleep(2 * 60)
     
