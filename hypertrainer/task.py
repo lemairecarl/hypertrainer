6065434664d02fcc77340484c25d16adbcdeb85b
Carl Lemaire <c.lemaire@live.ca>
2019-02-27 15:42:12 -0500

    Remove metrics handling for now

--- hypertrainer/task.py
+++ hypertrainer/task.py
@@ -1,11 +1,8 @@
 from pathlib import Path
 
-import pandas as pd
-from dataclasses import dataclass
 from ruamel_yaml import YAML
 
 from hypertrainer.computeplatform import LocalPlatform
-from hypertrainer.utils import get_item_at_path
 
 yaml = YAML()
 
@@ -23,63 +20,31 @@ class Task:
         self.config = yaml.load(config_file_path)
         self.name = self.config_file_path.stem
 
+    @property
+    def status_str(self):
+        # TODO more efficient
+        return self.platform.monitor(self)['status'].value
+
+    @property
+    def stdout_path(self):
+        path = Path(self.config['output_path']) / 'out.txt'
+        path.parent.mkdir(parents=True, exist_ok=True)
+        return path
+
+    @property
+    def stderr_path(self):
+        path = Path(self.config['output_path']) / 'err.txt'
+        path.parent.mkdir(parents=True, exist_ok=True)
+        return path
+
     def submit(self):
         self.job_id = self.platform.submit(self)
         self.task_id = self.platform.name + '_' + self.job_id
         
     def cancel(self):
         self.platform.cancel(self)
-        
-    @property
-    def status_str(self):
-        # TODO more efficient
-        return self.platform.monitor(self)['status'].value
-    
-    def refresh_metrics(self):
-        self.metrics.clear()
-        
-        # TODO use self.platform
-        # TODO train + val
-        output_path = Path(get_item_at_path(self.config, 'training.output_path'))
-    
-        # Loss
-        losses = pd.read_csv(output_path / 'trn_losses_values.log', sep=' ', header=None, names=['ep', 'val'])
-        self.metrics.append(
-            Metric(name='Loss', type='line', data=losses.values)
-        )
-        best_epoch = losses['val'].idxmin()
-        assert losses['ep'][best_epoch] == best_epoch
-        self.best_epoch = best_epoch
-    
-        # Classwise final score
-        all_scores = pd.read_csv(output_path / 'trn_classes_score.log', sep=' ', header=None, names=['ep', 'id', 'val'])
-        scores = all_scores[all_scores['ep'] == best_epoch].copy()
-        scores['metric'], scores['class_idx'] = scores['id'].str.split('_', 1).str
-        for name, val in scores.groupby('metric'):
-            self.metrics.append(
-                Metric(name=name, type='bar', data=val['val'].values)
-            )
-    
-    @property
-    def stdout_path(self):
-        path = Path(self.config['output_path']) / 'out.txt'
-        path.parent.mkdir(parents=True, exist_ok=True)
-        return path
-    
-    @property
-    def stderr_path(self):
-        path = Path(self.config['output_path']) / 'err.txt'
-        path.parent.mkdir(parents=True, exist_ok=True)
-        return path
     
     def get_output(self):
         # TODO use self.platform
         # return stdout, stderr as strings
         return self.stdout_path.read_text(), self.stderr_path.read_text()
-
-
-@dataclass
-class Metric:
-    name: str
-    type: str
-    data: object
