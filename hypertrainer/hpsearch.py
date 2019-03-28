cb23ef4649c41af2e02cf0e13c837ada0140cb2e
lemairecarl <c.lemaire@live.ca>
2019-03-28 17:20:45 -0400

    Make hpsearch.py usable from other scripts

--- hypertrainer/hpsearch.py
+++ hypertrainer/hpsearch.py
@@ -7,6 +7,8 @@ from ruamel.yaml import YAML
 
 from hypertrainer.utils import set_item_at_path
 
+yaml = YAML()
+
 ap = argparse.ArgumentParser()
 ap.add_argument('input_file', type=str, help='Path to yaml file')
 args = ap.parse_args()
@@ -27,11 +29,10 @@ def make_child_config(yaml_data):
     return child
 
 
-def generate_hpsearch():
+def generate(parent_yaml):
+    """Generate child YAML configs as a dict {name: yaml_object}"""
+
     # TODO documentation
-    yaml = YAML()
-    parent_file_path = Path(args.input_file)
-    parent_yaml = yaml.load(parent_file_path)
     hpsearch_config = parent_yaml['hpsearch']
     
     if hpsearch_config.get('is_child', False):
@@ -40,16 +41,27 @@ def generate_hpsearch():
     assert hpsearch_config['type'] == 'random_uniform'
     
     description = hpsearch_config['desc'] if 'desc' in hpsearch_config else 'hpsearch'
-    
+    child_configs = {}
+
     for trial_idx in range(hpsearch_config['n_trials']):
         child_yaml = make_child_config(parent_yaml)
         for p in hpsearch_config['params']:
             value = generate_random_value(p)
             set_item_at_path(child_yaml, p['param'], value)
-        child_file_path = parent_file_path.with_name(
-            parent_file_path.stem + '_{}_{}.yaml'.format(description, trial_idx))
+        name = parent_file_path.stem + '_{}_{}'.format(description, trial_idx)
+        child_configs[name] = child_yaml
+
+    return child_configs
+
+
+def write_to_file(child_configs, parent_file_path):
+    for name, child_yaml in child_configs.items():
+        child_file_path = parent_file_path.with_name(name + '.yaml')
         yaml.dump(child_yaml, child_file_path)
 
 
 if __name__ == '__main__':
-    generate_hpsearch()
+    parent_file_path = Path(args.input_file)
+    parent_yaml = yaml.load(parent_file_path)
+    child_configs = generate(parent_yaml)
+    write_to_file(child_configs, parent_file_path)
