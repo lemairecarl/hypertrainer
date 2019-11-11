4f381e4ab3add93738f45db147ffc569ccdc9992
lemairecarl <c.lemaire@live.ca>
2019-11-11 16:29:39 -0500

    Implement python worker start script

---
+++ worker.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import socket
+from multiprocessing import Process
+
+from redis import Redis
+from rq import Connection, Worker
+
+# Preload libraries
+# TODO import library_that_you_want_preloaded
+
+
+def work(queue_name):
+    w = Worker([queue_name])
+    w.work()
+
+
+if __name__ == '__main__':
+    hostname = socket.gethostname()
+    print('Hostname:', hostname)
+
+    with Connection(Redis(port=6380)):
+        specific_queue_worker = Process(target=work, args=(hostname,))
+        jobs_queue_worker = Process(target=work, args=('jobs',))
+
+        specific_queue_worker.start()
+        jobs_queue_worker.start()
+
+        specific_queue_worker.join()
+        jobs_queue_worker.join()
--- worker.sh
+++
@@ -1,1 +0,0 @@
-rq worker -u redis://localhost:6380 $(hostname) jobs
