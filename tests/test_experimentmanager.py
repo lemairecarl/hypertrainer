cc9bab693b312258b005d75944f4d65e19e4dafe
lemairecarl <c.lemaire@live.ca>
2020-02-12 11:30:51 -0500

    Start design of unit tests

---
+++ tests/test_experimentmanager.py
@@ -0,0 +1,20 @@
+def test_archive_task(client):
+    # 1. Submit local task
+    # 2. Archive task
+    # 3. Check that it exists
+    # 4. Check that it is in the archived category
+    pass  # TODO
+
+
+def test_delete_task(client):
+    # 1. Submit rq task
+    # 2. Archive task
+    # 3. Make sure task is archived
+    # 4. Delete task
+    # 5. Check that task does not exist in DB
+    # 6. Check that files on disk have been deleted
+    pass  # TODO
+
+
+def test_submit_rq_task(client):
+    pass  # TODO
