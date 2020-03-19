3b5bc409b16ae622ad6f284e3f3bf770c26ac5da
lemairecarl <c.lemaire@live.ca>
2020-03-19 17:01:50 -0400

    Make ExperimentManager a (kind of) singleton instead of a (kind of) namespace

--- hypertrainer/experimentmanager.py
+++ hypertrainer/experimentmanager.py
@@ -14,35 +14,39 @@ from hypertrainer.utils import yaml
 
 
 class ExperimentManager:
+    _instantiated = False
+
     platform_instances = None
 
-    @staticmethod
-    def init():
+    def __init__(self):
+        if ExperimentManager._instantiated:
+            raise Exception('ExperimentManager should not be instantiated manually. Use experiment_manager.')
+        ExperimentManager._instantiated = True
+
         # TODO initialize from config yaml instead of env vars
         # Instantiate ComputePlatform's if available
-        ExperimentManager.platform_instances = {
+        self.platform_instances = {
             ComputePlatformType.LOCAL: LocalPlatform()
         }
         if 'HTPLATFORM_WORKERS' in os.environ:
-            ExperimentManager.platform_instances[ComputePlatformType.HT] \
+            self.platform_instances[ComputePlatformType.HT] \
                 = HtPlatform(os.environ['HTPLATFORM_WORKERS'].split(','))
         else:
-            ExperimentManager.platform_instances[ComputePlatformType.HT] \
+            self.platform_instances[ComputePlatformType.HT] \
                 = HtPlatform(['localhost'])  # FIXME
         if 'GRAHAM' in os.environ:
-            ExperimentManager.platform_instances[ComputePlatformType.GRAHAM] \
+            self.platform_instances[ComputePlatformType.GRAHAM] \
                 = SlurmPlatform(server_user=os.environ['GRAHAM'])
         if 'BELUGA' in os.environ:
-            ExperimentManager.platform_instances[ComputePlatformType.BELUGA] \
+            self.platform_instances[ComputePlatformType.BELUGA] \
                 = SlurmPlatform(server_user=os.environ['BELUGA'])
 
-    @staticmethod
-    def get_tasks(platform: Optional[ComputePlatformType] = None,
+    def get_tasks(self, platform: Optional[ComputePlatformType] = None,
                   proj: Optional[str] = None,
                   archived=False) -> List[Task]:
         # TODO rename this function? Maybe get_filtered_tasks?
         p_list = [platform] if platform is not None else None
-        ExperimentManager.update_tasks(platforms=p_list)  # TODO return tasks to avoid other db query?
+        self.update_tasks(platforms=p_list)  # TODO return tasks to avoid other db query?
 
         if platform is None:
             q = Task.select().where(Task.is_archived == archived)
@@ -55,23 +59,21 @@ class ExperimentManager:
         tasks = list(q)
 
         for t in tasks:
-            ExperimentManager.monitor(t)
+            self.monitor(t)
         return tasks
 
-    @staticmethod
-    def update_tasks(platforms: list = None):
+    def update_tasks(self, platforms: list = None):
         if platforms is None:
-            platforms = ExperimentManager.list_platforms()
+            platforms = self.list_platforms()
         for ptype in platforms:
-            platform = ExperimentManager.platform_instances[ptype]
+            platform = self.platform_instances[ptype]
             tasks = list(Task.select().where(Task.platform_type == ptype))
             if len(tasks) == 0:
                 continue
             platform.update_tasks(tasks)
             Task.bulk_update(tasks, Task.get_fields())  # FIXME updating all records everytime is heavy
 
-    @staticmethod
-    def create_tasks(platform: str, config_file: str, project: str = ''):
+    def create_tasks(self, platform: str, config_file: str, project: str = ''):
         # Load yaml config
         config_file_path = Path(config_file)
         yaml_config = yaml.load(config_file_path)
@@ -96,44 +98,37 @@ class ExperimentManager:
             tasks.append(t)
         # Submit tasks
         for t in tasks:
-            ExperimentManager.submit_task(t)  # TODO submit in batch to server!
+            self.submit_task(t)  # TODO submit in batch to server!
         return tasks
 
-    @staticmethod
-    def submit_task(task: Task):
-        task.job_id = ExperimentManager.get_platform(task).submit(task)
+    def submit_task(self, task: Task):
+        task.job_id = self.get_platform(task).submit(task)
         task.post_submit()
 
-    @staticmethod
-    def get_tasks_by_id(task_ids: list):
+    def get_tasks_by_id(self, task_ids: list):
         return list(Task.select().where(Task.id.in_(task_ids)))
 
-    @staticmethod
-    def resume_tasks(tasks: Iterable[Task]):
+    def resume_tasks(self, tasks: Iterable[Task]):
         for t in tasks:
             if not t.status.is_active():
-                t.job_id = ExperimentManager.get_platform(t).submit(t, resume=True)  # TODO one bulk ssh command
+                t.job_id = self.get_platform(t).submit(t, resume=True)  # TODO one bulk ssh command
                 t.post_resume()
 
-    @staticmethod
-    def cancel_tasks(tasks: Iterable[Task]):
+    def cancel_tasks(self, tasks: Iterable[Task]):
         for t in tasks:
             if t.status.is_active():
-                ExperimentManager.get_platform(t).cancel(t)  # TODO one bulk ssh command
+                self.get_platform(t).cancel(t)  # TODO one bulk ssh command
                 t.post_cancel()
 
-    @staticmethod
-    def monitor(t: Task):
+    def monitor(self, t: Task):
         # TODO rename this method 'update' or something?
-        t.logs = ExperimentManager.get_platform(t).fetch_logs(t)
+        t.logs = self.get_platform(t).fetch_logs(t)
         t.interpret_logs()
 
-    @staticmethod
-    def archive_tasks_by_id(task_ids: list):
+    def archive_tasks_by_id(self, task_ids: list):
         Task.update(is_archived=True).where(Task.id.in_(task_ids)).execute()
 
-    @staticmethod
-    def delete_tasks_by_id(task_ids: list):
+    def delete_tasks_by_id(self, task_ids: list):
         if Task.select().where(Task.id.in_(task_ids) & (Task.is_archived == False)).count() > 0:
             raise RuntimeError('Only archived tasks can be deleted')
         for t in Task.select().where(Task.id.in_(task_ids)):
@@ -142,19 +137,16 @@ class ExperimentManager:
                           onerror=lambda function, path, excinfo: print('ERROR', function, path, excinfo))
         Task.delete().where(Task.id.in_(task_ids)).execute()
 
-    @staticmethod
-    def list_projects():
+    def list_projects(self):
         return [t.project for t in Task.select(Task.project).where(Task.project != '').distinct()]
 
-    @staticmethod
-    def get_platform(task: Task):
+    def get_platform(self, task: Task):
         try:
-            return ExperimentManager.platform_instances[task.platform_type]
+            return self.platform_instances[task.platform_type]
         except KeyError:
             raise Exception(f'The platform {task.platform_type} has not been initialized.')
 
-    @staticmethod
-    def list_platforms(as_str=False):
+    def list_platforms(self, as_str=False):
         """Lists available platforms.
 
         Use this instead of list(ComputePlatformType), as it would contain all *implemented* platforms, including those
@@ -162,10 +154,9 @@ class ExperimentManager:
         """
 
         if as_str:
-            return [p.value for p in ExperimentManager.platform_instances.keys()]
+            return [p.value for p in self.platform_instances.keys()]
         else:
-            return list(ExperimentManager.platform_instances.keys())
+            return list(self.platform_instances.keys())
 
 
-experiment_manager = ExperimentManager()  # FIXME do not instantiate, it is just a namespace
-experiment_manager.init()
+experiment_manager = ExperimentManager()
